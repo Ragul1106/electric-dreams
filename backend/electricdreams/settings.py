c5db35f7791ee0869f1856adaabaf7700c6f990d
Ragul116 <ragulranjith1106@gmail.com>
2025-10-01 21:37:05 +0530

    add backend url

--- backend/electricdreams/settings.py
+++ backend/electricdreams/settings.py
@@ -147,6 +147,17 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
