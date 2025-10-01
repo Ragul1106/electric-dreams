3ef8e91fe448bc9cbb0fa04aeadd3315f7e56423
Ragul116 <ragulranjith1106@gmail.com>
2025-10-01 13:37:36 +0530

    initial commit

--- backend/electricdreams/settings.py
+++ backend/electricdreams/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rj%eyvbke(c4v_eu*in^e@x842n#*&#!*s&vbq9ki0pncluese
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
